20d76a96fcb21142d939389ba8cc20d1a7404970
ImaniJo-code <imani.l.johnson@gmail.com>
2021-07-04 20:17:47 -0400

    set global variables

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// set variables 
+var locations = [];
+
+var inputEl = document.getElementById("location")
+var searchEl = document.getElementById("button");
+var weatherContainerEl = document.getElementById("current-forecast-container");
+var locationSearchInputEl = document.getElementById("searched-location");
+var forecastEl = document.getElementById("forecastTitle");
