13d1dbaa5fb0dd2682acb1f71d5f725a4152f6f1
ImaniJo-code <imani.l.johnson@gmail.com>
2021-07-04 20:26:23 -0400

    get and save location searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,22 @@ var searchEl = document.getElementById("button");
 var weatherContainerEl = document.getElementById("current-forecast-container");
 var locationSearchInputEl = document.getElementById("searched-location");
 var forecastEl = document.getElementById("forecastTitle");
+
+// get and save location searches to local storage
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    var location = inputEl.value.trim();
+    if(location){
+        getCityWeather(location);
+        get5Day(location);
+        locations.unshift({location});
+        inputEl.value = "";
+    }
+    saveSearch();
+    pastSearch(location);
+}
+
+var saveSearch = function(){
+    localStorage.setItem("locations", JSON.stringify(locations));
+};
+
