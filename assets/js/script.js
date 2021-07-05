04f125caa47b9233a18ff938ea539c190e83c682
ImaniJo-code <imani.l.johnson@gmail.com>
2021-07-04 23:33:56 -0400

    api key and api url added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,45 @@
-// set variables 
+// set variables
 var locations = [];
 
-var inputEl = document.getElementById("location")
-var searchEl = document.getElementById("button");
-var weatherContainerEl = document.getElementById("current-forecast-container");
-var locationSearchInputEl = document.getElementById("searched-location");
-var forecastEl = document.getElementById("forecastTitle");
+var locationFormEl=document.getElementById("location-form");
+var locationInputEl=document.getElementById("location");
+var forecastContainerEl=document.getElementById("current-forecast-container");
+var locationSearchInputEl = document.getElementById("selected-location");
+var forecastTitle = document.getElementById("forecast");
+var fivedayContainerEl = document.getElementByIdr("fiveday-container");
+var pastlocationButtonEl = document.getElementById("past-location-btns");
 
-// get and save location searches to local storage
-var formSubmitHandler = function(event){
-    event.preventDefault();
-    var location = inputEl.value.trim();
-    if(location){
-        getCityWeather(location);
-        get5Day(location);
-        locations.unshift({location});
-        inputEl.value = "";
+var formSumbitHandler = function(event){
+  // prevent page from refreshing  
+  event.preventDefault();
+
+  // get value from input element 
+  var location = locationInputEl.value.trim();
+  
+  if(location){
+    getLocationForecast(location);
+    get5DayForecast(location);
+    
+    // clear old content
+    locations.unshift({location});
+    locationInputEl.value = "";
+  } else{
+    alert("Please enter a City");
+  }
+};
+
+var getLocationForecast = function(location){
+  var apiKey = "33bdcf610639e5013b3b8d2a63fcb5b9"
+  var apiURL = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}"
+
+  // make a get request to url
+  fetch(apiURL).then(function(response){
+    // request was successful
+    if (response.ok){
+      response.json().then(function(data){
+        displayWeather(data, location);
+      });
     }
-    saveSearch();
-    pastSearch(location);
-}
-
-var saveSearch = function(){
-    localStorage.setItem("locations", JSON.stringify(locations));
+  });
 };
 
